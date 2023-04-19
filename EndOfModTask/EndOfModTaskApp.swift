993523d94c288f97ea20a4478bb4217070b3b685
Alistair Fraser <afraser@lcs.on.ca>
2023-04-19 12:00:40 -0400

    Initial Commit

---
+++ EndOfModTask/EndOfModTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  EndOfModTaskApp.swift
+//  EndOfModTask
+//
+//  Created by Alistair Fraser on 2023-04-19.
+//
+
+import SwiftUI
+
+@main
+struct EndOfModTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
